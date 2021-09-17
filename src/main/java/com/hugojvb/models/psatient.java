bb2e8cc76b679b56940a142e3d766eaa6ba412d7
hugojvb <hugojvb20@gmail.com>
2021-09-17 14:30:08 +0100

    Changed patient file

--- src/main/java/com/hugojvb/models/psatient.java
+++ src/main/java/com/hugojvb/models/psatient.java
@@ -19,13 +19,14 @@ import lombok.NoArgsConstructor;
 public class patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "first_name", nullable = false)
-    private String first_name;
+    private String firstName;
 
     @Column(name = "last_name", nullable = false)
-    private String last_name;
+    private String lastName;
 
     @Column(name = "age", nullable = false)
     private Integer age;
