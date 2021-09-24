4b2cfe68b16e620d2a7e5836420b1a35aa6d2360
hugojvb <hugojvb20@gmail.com>
2021-09-24 20:15:46 +0100

    Changed patient model and create patient markup

--- src/main/java/com/hugojvb/patientmanagement/models/Patient.java
+++ src/main/java/com/hugojvb/patientmanagement/models/Patient.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -22,12 +23,18 @@ public class Patient {
 	@Column(name = "id")
 	private Long id;
 
-	@Column(name = "first_name", nullable = false)
-	private String firstName;
-
-	@Column(name = "last_name", nullable = false)
-	private String lastName;
+	@Column(name = "name", nullable = false)
+	private String name;
 
 	@Column(name = "age", nullable = false)
 	private Integer age;
+
+	@Column(name = "state")
+	private Integer state;
+
+	@Column(name = "scholarity")
+	private Integer scholarity;
+
+	// @OneToMany
+	// private List<Observation> observations;
 }
