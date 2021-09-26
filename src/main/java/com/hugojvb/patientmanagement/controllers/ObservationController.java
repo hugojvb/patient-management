ac592abfac9bd1419c42047e2649ce7fdb40cd74
hugojvb <hugojvb20@gmail.com>
2021-09-26 15:24:53 +0100

    Added observation controller

---
+++ src/main/java/com/hugojvb/patientmanagement/controllers/ObservationController.java
@@ -0,0 +1,26 @@
+package com.hugojvb.patientmanagement.controllers;
+
+import com.hugojvb.patientmanagement.models.Observation;
+import com.hugojvb.patientmanagement.models.Patient;
+import com.hugojvb.patientmanagement.services.PatientService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ObservationController {
+
+	@Autowired
+	private PatientService patientService;
+
+	@GetMapping("/patients/{id}/observations/add")
+	public String addObservation(@PathVariable Long id, Model model) {
+		Observation newObservation = new Observation();
+		newObservation.setPatient(patientService.getPatientById(id));
+		model.addAttribute("observation", newObservation);
+		return "add_observation";
+	}
+}
