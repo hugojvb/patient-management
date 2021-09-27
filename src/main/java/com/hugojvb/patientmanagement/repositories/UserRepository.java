2f1bf17e2c2ab757a8d0750de769c26b4847bbd5
hugojvb <hugojvb20@gmail.com>
2021-09-27 22:59:06 +0100

    Added user repository

---
+++ src/main/java/com/hugojvb/patientmanagement/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.hugojvb.patientmanagement.repositories;
+
+import com.hugojvb.patientmanagement.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
