092002b33b825b678ed7109546a84f335f04a5a0
hugojvb <hugojvb20@gmail.com>
2021-09-28 19:28:11 +0100

    Changed user registration controller

--- src/main/java/com/hugojvb/patientmanagement/controllers/UserRegistration.java
+++ src/main/java/com/hugojvb/patientmanagement/controllers/UserRegistration.java
@@ -5,20 +5,29 @@ import com.hugojvb.patientmanagement.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Controller
+@RequestMapping("/registration")
 public class UserRegistration {
 
 	@Autowired
 	private UserService userService;
 
-	@GetMapping("/registration")
-	public String regusterUser(@ModelAttribute("user") UserRegistrationDto userRegistrationDto) {
+	@GetMapping
+	public String showRegistrationForm(Model model) {
+		return "registration";
+	}
+
+	@PostMapping
+	public String registerUser(@ModelAttribute("user") UserRegistrationDto userRegistrationDto) {
 		userService.saveUser(userRegistrationDto);
 
 		return "redirect:/registration?success";
