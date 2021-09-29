f6304c7384a7b7918bc11389b60090e66fc47bac
hugojvb <hugojvb20@gmail.com>
2021-09-29 23:53:39 +0100

    Added loadUserByUsername method

--- src/main/java/com/hugojvb/patientmanagement/services/Impl/UserServiceImpl.java
+++ src/main/java/com/hugojvb/patientmanagement/services/Impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.hugojvb.patientmanagement.repositories.UserRepository;
 import com.hugojvb.patientmanagement.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,5 +23,14 @@ public class UserServiceImpl implements UserService {
 		newUser.setEmail(userRegistrationDto.getEmail());
 		newUser.setPassword(userRegistrationDto.getPassword());
 		return userRepository.save(newUser);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("invalid email or password");
+		}
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), null);
 	}
 }
