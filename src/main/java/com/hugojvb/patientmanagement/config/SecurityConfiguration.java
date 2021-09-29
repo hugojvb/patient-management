52401aceaccca63f5cd1189618d203acdf5fd83b
hugojvb <hugojvb20@gmail.com>
2021-09-29 20:37:21 +0100

    Changed auth configuration

--- src/main/java/com/hugojvb/patientmanagement/config/SecurityConfiguration.java
+++ src/main/java/com/hugojvb/patientmanagement/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -11,7 +12,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeRequests().antMatchers("/registration**", "/css/**").permitAll();
+		httpSecurity.authorizeRequests().antMatchers("/registration**", "/css/**").permitAll().anyRequest()
+				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.invalidateHttpSession(true).clearAuthentication(true)
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
+				.permitAll();
 	}
 
 }
