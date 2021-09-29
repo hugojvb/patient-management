c7eaf8dfee3df032727c7d344ab1363beb559015
hugojvb <hugojvb20@gmail.com>
2021-09-29 19:41:35 +0100

    Added configuration

---
+++ src/main/java/com/hugojvb/patientmanagement/config/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.hugojvb.patientmanagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeRequests().antMatchers("/registration**", "/css/**").permitAll();
+	}
+
+}
