43d33f4400b0aa1fa8bff10964ba5b6032c0fbde
hugojvb <hugojvb20@gmail.com>
2021-09-30 20:14:38 +0100

    Changed granted authority role

--- src/main/java/com/hugojvb/patientmanagement/services/Impl/UserServiceImpl.java
+++ src/main/java/com/hugojvb/patientmanagement/services/Impl/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.hugojvb.patientmanagement.services.Impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.hugojvb.patientmanagement.dto.UserRegistrationDto;
 import com.hugojvb.patientmanagement.models.User;
 import com.hugojvb.patientmanagement.repositories.UserRepository;
 import com.hugojvb.patientmanagement.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +24,8 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 
+	List<GrantedAuthority> role_name = new ArrayList<>();
+
 	@Override
 	public User saveUser(UserRegistrationDto userRegistrationDto) {
 		User newUser = new User();
@@ -30,11 +36,11 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = userRepository.findByEmail(username);
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(email);
 		if (user == null) {
 			throw new UsernameNotFoundException("invalid email or password");
 		}
-		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), null);
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), role_name);
 	}
 }
